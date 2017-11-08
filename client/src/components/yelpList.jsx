e353e71ddfe3bf81aec316cd02bc2b00d3df4b5f
Erica H. Kim <EricaHKim@Ericas-MacBook-Pro.local>
2017-11-08 13:44:42 -0800

    [add] Work on group drop down button

--- client/src/components/yelpList.jsx
+++ client/src/components/yelpList.jsx
@@ -3,35 +3,56 @@ import React from 'react';
 class YelpList extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      showGroups = false
+    };
+
+    this.groupClick = this.groupClick.bind(this);
+    }
+
+    groupClick (e) {
+      this.setState({
+        showGroups = true
+      })
     }
 
   render() {
+
     return (
     <div>
       <div classNameName="dropdown">
         <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown button
+          Groups
         </button>
-        <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-          <a className="dropdown-item" href="#">Action</a>
-          <a className="dropdown-item" href="#">Another action</a>
-          <a className="dropdown-item" href="#">Something else here</a>
+        <div  onClick={this.groupClick} className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <a className="dropdown-item">Action</a>
+          {/* <a className="dropdown-item" href="#">Another action</a>
+          <a className="dropdown-item" href="#">Something else here</a> */}
         </div>
       </div>
 
 
       <form className="form-inline">
-        <label className="sr-only" for="inlineFormInput">Type</label>
-        <input type="text" class="form-control mb-2 mr-sm-2 mb-sm-0" id="inlineFormInput" placeholder="type" />
+        <label className="sr-only" htmlFor="inlineFormInput">Type</label>
+        <input type="text" className="form-control mb-2 mr-sm-2 mb-sm-0" id="inlineFormInput" placeholder="type" />
 
-        <label className="sr-only" for="inlineFormInputGroup">Location</label>
+        <label className="sr-only" htmlFor="inlineFormInputGroup">Location</label>
         <div className="input-group mb-2 mr-sm-2 mb-sm-0">
-          <input type="text" class="form-control" id="inlineFormInputGroup" placeholder="location" />
+          <input type="text" className="form-control" id="inlineFormInputGroup" placeholder="location" />
         </div>
 
-        
+        <div className="btn-group" htmlFor="inlineFormInputGroup">
+          <button type="button" className="btn btn-danger">cost</button>
+          <button type="button" className="btn btn-danger dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <span className="sr-only">Toggle Dropdown</span>
+          </button>
+          <div className="dropdown-menu">
+            <a className="dropdown-item" href="#">$$$</a>
+          </div>
+        </div>
 
-        <button type="submit" class="btn btn-primary">Search</button>
+        <button type="submit" className="btn btn-primary" >Search</button>
       </form>
 
     </div>
