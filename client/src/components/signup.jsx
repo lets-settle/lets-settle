9b1946c95b28f96ca13d3d04e7c9367d6241f20f
Jamie <jamie.lo112@gmail.com>
2017-11-08 10:24:37 -0800

    [add] sign up form currently rendering

--- client/src/components/signup.jsx
+++ client/src/components/signup.jsx
@@ -4,80 +4,88 @@ class Signup extends React.Component {
   constructor(props) {
     super(props);
       this.state = {
-        fullName = '',
-        username = '',
-        password = '',
-        email = ''
+        fullname: '',
+        username: '',
+        password: '',
+        email: ''
       }
 
       this.changeName = this.changeName.bind(this);
       this.changeUsername = this.changeUsername.bind(this);
+      this.changePassword = this.changePassword.bind(this);
+      this.changeEmail = this.changeEmail.bind(this);
       this.signUpSubmit = this.signUpSubmit.bind(this);
     }
 
-    changeName(value) {
+    changeName (value) {
       this.setState({
-        fullname: value.target.value()
+        fullname: value.target.value
+      })
+    }
+    changeUsername (value) {
+      this.setState({
+        username: value.target.value
+      })
+    }
+    changePassword (value) {
+      this.setState({
+        password: value.target.value
+      })
+    }
+    changeEmail (value) {
+      this.setState({
+        email: value.target.value
       })
     }
     
-    signUpSubmit() {
+    signUpSubmit () {
+      let form = this;
+
+      console.log('Form submitted')
       axios.post('/signup', {
-        name: this.fullName,
-        username: this.username,
-        password: this.password,
-        email: this.email
+        name: form.state.fullname,
+        username: form.state.username,
+        password: form.state.password,
+        email: form.state.email
       }).then(response => {
         console.log(response);
       }).catch(err => {
         console.log(err)
       })
 
-      this.setState({
-        fullName: '',
-        username: '',
-        password: '',
-        email: ''
-      })
     }
 
   render() {
     return (
     <div>
-      <form class="form-horizontal">
-        <div class="form-group">
-          <label for="inputName" class="col-sm-2 control-label">Full Name</label>
-          <div class="col-sm-10">
-            <input class="form-control" id="inputName" placeholder="Full Name" value={this.state.fullname} onChange={this.changeName}> 
+      <form className="form-horizontal">
+        <div className="form-group">
+          <label for="inputName" className="col-sm-2 control-label">Full Name</label>
+          <div className="col-sm-10">
+            <input className="form-control" id="inputName" placeholder="Full Name" value={this.state.fullname} onChange={this.changeName}/> 
           </div>
         </div>
-        <div class="form-group">
-          <label for="inputUsername" class="col-sm-2 control-label">Username</label>
-          <div class="col-sm-10">
-            <input class="form-control" id="inputUsername" placeholder="Username">
+        <div className="form-group">
+          <label for="inputUsername" className="col-sm-2 control-label">Username</label>
+          <div className="col-sm-10">
+            <input className="form-control" id="inputUsername" placeholder="Username" value={this.state.username} onChange={this.changeUsername}/>
           </div>
         </div>
-        <div class="form-group">
-          <label for="inputPassword" class="col-sm-2 control-label">Password</label>
-          <div class="col-sm-10">
-            <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+        <div className="form-group">
+          <label for="inputPassword" className="col-sm-2 control-label">Password</label>
+          <div className="col-sm-10">
+            <input type="password" className="form-control" id="inputPassword" placeholder="Password" value={this.state.password} onChange={this.changePassword}/>
           </div>
         </div>
-        <div class="form-group">
-          <label for="inputPwConfirmation" class="col-sm-2 control-label">Password Confirmation</label>
-          <div class="col-sm-10">
-            <input type="password" class="form-control" id="inputPwConfirmation" placeholder="Password">
+        <div className="form-group">
+          <label for="inputEmail" className="col-sm-2 control-label">Email</label>
+          <div className="col-sm-10">
+            <input type="email" className="form-control" id="inputEmail" placeholder="Email" value={this.state.email} onChange={this.changeEmail}/>
           </div>
         </div>
-        <div class="form-group">
-          <label for="inputEmail" class="col-sm-2 control-label">Email</label>
-          <div class="col-sm-10">
-            <input type="email" class="form-control" id="inputEmail" placeholder="Email">
-          </div>
-        </div>
-        <div class="form-group">
-          <div class="col-sm-offset-2 col-sm-10">
-            <button type="submit" class="btn btn-default" onClick="">Sign Up</button>
+        <div className="form-group">
+          <div className="col-sm-offset-2 col-sm-10">
+            <button type="submit" className="btn btn-default" onClick={this.signUpSubmit}>Sign Up</button>
           </div>
         </div>
       </form>
