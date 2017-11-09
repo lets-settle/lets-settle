a764f08a71b2a7b053e68f45ae1e11015e5f3e06
Jerry Tang <jerrytang@Jerrys-MacBook-Pro.local>
2017-11-08 19:08:27 -0800

    finish setting /signup post request and which insert user info successuflly into databse

--- database/models/model.js
+++ database/models/model.js
@@ -1,2 +1,43 @@
+var db = require('../index.js')
+const Sequelize = require('sequelize');
+
 //define tables here 
 //index.js is for connecting to database
+var User = db.define('user', {
+  name: Sequelize.STRING,
+  username: Sequelize.STRING,
+  password: Sequelize.STRING,
+  email: Sequelize.STRING,
+});
+
+var Group = db.define('group', {
+  group_name: Sequelize.STRING
+});
+
+var UserGroup = db.define('usergroup', {
+  userid: Sequelize.INTEGER,
+  groupid: Sequelize.INTEGER
+});
+
+
+db.sync().then(function() {
+    return User.create({
+      name: 'Jerry',
+      username: 'Tang',
+      password: 'jt111588',
+      email: 'jerica0722@gmail.com'
+    })
+  }).then(function() {
+    return Group.create({
+      group_name: 'theTestGroup'
+    });
+  }).then(function() {
+    return UserGroup.create({
+      userid:1,
+      groupid:1
+    })
+  })
+  
+exports.User = User;
+exports.Group = Group;
+exports.UserGroup = UserGroup;
--- server/index.js
+++ server/index.js
@@ -3,15 +3,32 @@ let app = express();
 // let db = require('../database/index');
 let bodyParser = require('body-parser');
 let axios = require('axios');
+let model = require('../database/models/model.js');
+//let routes = require('./routes/routes.js');
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-
+app.use(bodyParser.urlencoded({ extended: true }))
 // parse application/json
 app.use(bodyParser.json())
 
 app.use(express.static(__dirname + '/../client/dist'));
 
+app.post('/signup', function(req, res) {
+  var name = req.body.name;
+  var username = req.body.username;
+  var password = req.body.password;
+  var email = req.body.email; 
+ 
+  model.User.create({
+    name: name,
+    username: username,
+    password: password,
+    email: email
+  }).then(function(user) {
+    console.log(user)
+    res.send('user inserted into database!')
+  })
+}) 
 
 let port = 1128;
 
@@ -19,4 +36,3 @@ app.listen(port, function() {
 
   console.log(`listening on port ${port}`);
 });
-
