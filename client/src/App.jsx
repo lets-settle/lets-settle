66470a602801adedde82533b412bd1985dad9d45
“Smita <smitajbhakta@gmail.com>
2017-11-09 16:04:25 -0800

    [add] NavBar add

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import $ from 'jquery';
+const $ = require('jquery');
 import Login from './components/Login.jsx';
 import axios from 'axios';
 import bodyParser from 'body-parser';
+
+
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { 
-      groups: []
+      isLoggedIn: false
     }
   }
+
+  loggedIn() {
+    this.setState({
+      isLoggedIn: true
+    });  
+}
+
 
   render () {
     return (
     <div>
-    {/* <img id='title' src = {require('../dist/images/logo.png')}/> */}
-        This goes to homepage or login
-      {<Login />}
+    {/* <img id='title' src = {logo}/> */}
+      {<Login isLoggedIn = {this.loggedIn}/>}
     </div>
     )
   }
