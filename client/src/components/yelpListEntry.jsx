e4b653eae3b0dbe8ce751ff97ad486db38f8b0df
Erica H. Kim <EricaHKim@Ericas-MacBook-Pro.local>
2017-11-09 19:50:23 -0800

    [rebase] Working on group suggestion selection

--- client/src/components/yelpListEntry.jsx
+++ client/src/components/yelpListEntry.jsx
@@ -11,16 +11,26 @@ class YelpListEntry extends React.Component {
     };
 
     this.changeColor = this.changeColor.bind(this);
+    this.suggestionSelected = this.suggestionSelected.bind(this);
   }
 
-  changeColor(){
-    this.setState({color: !this.state.color})
+  changeColor() {
+    this.setState({
+      color: !this.state.color
+    })
   };
+
+  suggestionSelected(e) {
+    this.setState({
+      suggestion: e.target.value
+    })
+    console.log('SUGGESTIONNN', this.state.suggestion);
+  }
 
   render() {
     let bgColor = this.state.color ? "white" : "green"
     return (
-    <div style={{backgroundColor: bgColor}} onClick={this.changeColor} value={this.state.suggestion}>
+    <div style={{backgroundColor: bgColor}} value={this.state.suggestion}  onClick={this.changeColor}>
      {this.props.suggestion}
     </div>
     ) 
