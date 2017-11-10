a6680fc249813588addafef19791aa7e2433c4fe
Jamie <jamie.lo112@gmail.com>
2017-11-09 20:59:15 -0800

    [add] gitignore

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -37,34 +37,34 @@ class Login extends React.Component {
       
       console.log('Im logging in', this.state.email, this.state.password);
 
-      firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
-        .then(function() {
-          // Existing and future Auth states are now persisted in the current
-          // session only. Closing the window would clear any existing state even
-          // if a user forgets to sign out.
-          // ...
-          // New sign-in will be persisted with session persistence.
-          return firebase.auth().signInWithEmailAndPassword(email, password);
-        })
-        .catch(function(error) {
-          // Handle Errors here.
-          var errorCode = error.code;
-          var errorMessage = error.message;
-        });
-      
-      // firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-      //   // Handle Errors here.
-      //   const errorCode = error.code;
-      //   const errorMessage = error.message;
-      //   console.log('Login Error!', errorCode, errorMessage)
-      // }).then((result) => {
-      //   console.log('im in login: ', result);
-      //   console.log('im in login2: ', result.user);
-      //   // const user = result.user;
-      //   // this.setState({
-      //     //   user
-      //     // });
+      // firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+      //   .then(function() {
+      //     // Existing and future Auth states are now persisted in the current
+      //     // session only. Closing the window would clear any existing state even
+      //     // if a user forgets to sign out.
+      //     // ...
+      //     // New sign-in will be persisted with session persistence.
+      //     return firebase.auth().signInWithEmailAndPassword(email, password);
+      //   })
+      //   .catch(function(error) {
+      //     // Handle Errors here.
+      //     var errorCode = error.code;
+      //     var errorMessage = error.message;
       //   });
+      
+      firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log('Login Error!', errorCode, errorMessage)
+      }).then((result) => {
+        console.log('im in login: ', result);
+        console.log('im in login2: ', result.user);
+        // const user = result.user;
+        // this.setState({
+          //   user
+          // });
+        });
         
       axios.post('/api/login', {
         email: this.state.email
